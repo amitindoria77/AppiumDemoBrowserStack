5eec23a31357b19f972296111cffc5bfae42f3f2
amitindoria77 <83124508+amitindoria77@users.noreply.github.com>
2025-02-12 16:08:38 +0000

    Update Startup.cs

--- AppiumCSharp/Utils/Startup.cs
+++ AppiumCSharp/Utils/Startup.cs
@@ -5,12 +5,30 @@ namespace AppiumCSharp
 {
     public static class Startup
     {
-        private static IConfiguration ConfigurePlatform() =>
-            new ConfigurationBuilder()
-            .AddJsonFile(@$"ConfigFiles\appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Android"}.json")
-            .Build();
+          private static IConfigurationRoot ConfigurePlatform { get; set; }
 
-     
+  static Startup()
+  {
+      var basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      var configPath = Path.Combine(basePath, "ConfigFiles", "appsettings.Android.json");
+
+      if (!File.Exists(configPath))
+      {
+          throw new FileNotFoundException($"The configuration file '{configPath}' was not found and is not optional.");
+      }
+
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(basePath)
+          .AddJsonFile(configPath, optional: false, reloadOnChange: true);
+
+      ConfigurePlatform = builder.Build();
+  }
+
+  //private static IConfiguration ConfigurePlatform() =>
+  //    new ConfigurationBuilder()
+  //        .AddJsonFile(@$"ConfigFiles\appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Android"}.json")
+  //        .Build();
+
       
         public static string ReadFromAppSettings(string value) => ConfigurePlatform().GetSection(value).Value;
     }

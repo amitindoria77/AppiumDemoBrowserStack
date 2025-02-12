629dfbdae8b1b1fe6e4d97c6b090d2060ca251f9
amitindoria77 <83124508+amitindoria77@users.noreply.github.com>
2025-02-12 16:14:48 +0000

    Update Startup.cs

--- AppiumCSharp/Utils/Startup.cs
+++ AppiumCSharp/Utils/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 
@@ -6,31 +6,28 @@ namespace AppiumCSharp
 {
     public static class Startup
     {
-          private static IConfigurationRoot ConfigurePlatform { get; set; }
+        private static IConfigurationRoot ConfigurePlatform { get; set; }
 
-  static Startup()
-  {
-      var basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-      var configPath = Path.Combine(basePath, "ConfigFiles", "appsettings.Android.json");
+        static Startup()
+        {
+            var basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var configPath = Path.Combine(basePath, "ConfigFiles", "appsettings.Android.json");
 
-      if (!File.Exists(configPath))
-      {
-          throw new FileNotFoundException($"The configuration file '{configPath}' was not found and is not optional.");
-      }
+            if (!File.Exists(configPath))
+            {
+                throw new FileNotFoundException($"The configuration file '{configPath}' was not found and is not optional.");
+            }
 
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(basePath)
-          .AddJsonFile(configPath, optional: false, reloadOnChange: true);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile(configPath, optional: false, reloadOnChange: true);
 
-      ConfigurePlatform = builder.Build();
-  }
+            ConfigurePlatform = builder.Build();
+        }
 
-  //private static IConfiguration ConfigurePlatform() =>
-  //    new ConfigurationBuilder()
-  //        .AddJsonFile(@$"ConfigFiles\appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Android"}.json")
-  //        .Build();
-
-      
-        public static string ReadFromAppSettings(string value) => ConfigurePlatform().GetSection(value).Value;
+        public static string ReadFromAppSettings(string value)
+        {
+            return ConfigurePlatform.GetSection(value).Value;
+        }
     }
 }
